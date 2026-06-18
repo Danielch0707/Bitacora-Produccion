79c719c2974c85f374eb838ce1f260d0a0537754
Danielch0707 <dchavez01@lear.com>
2026-06-18 14:31:40 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,7 @@
 streamlit run app.py \
 --server.port $PORT \
 --server.address 0.0.0.0 \
+--server.baseUrlPath "" \
 --server.enableCORS false \
---server.enableXsrfProtection false
+--server.enableXsrfProtection false \
+--browser.gatherUsageStats false
