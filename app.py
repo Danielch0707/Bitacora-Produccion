5acca1b54250350a700f3e4517022949cdb0b2e4
Danielch0707 <dchavez01@lear.com>
2026-06-18 11:37:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
 import streamlit as st
 
 st.title("✅ TEST FUNCIONANDO")
-
-archivo = st.file_uploader("Sube archivo")
-
-if archivo is not None:
-    st.success("Archivo cargado")
-else:
-    st.warning("Sube archivo")
